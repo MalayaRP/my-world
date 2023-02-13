804194f2e31df2f31b52a224515dfe372f41d97f
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-02-13 15:43:02 +0530

    Update page.js

--- app/courses/page.js
+++ app/courses/page.js
@@ -11,7 +11,9 @@ import {
   } from '@chakra-ui/react'
 import {CheckCircleIcon} from "@chakra-ui/icons"
 export default function page() {
-  return (
+  return (if (typeof window !== "undefined") {
+  // Client-side-only code
+
     <HStack spacing="10px">
         <Box>
         <Image src={require("../../public/course2.gif")} style={{margin:"left", height:"400px", width:"400px"}} alt="reading"/>
@@ -32,7 +34,7 @@ export default function page() {
   </ListItem>
 </List>
         </Box>
-    </HStack>
+    </HStack>}
       
   )
 }
