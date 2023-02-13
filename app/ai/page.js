33430d78d7e4471cf0f7287c9cb5b5dd9b42a88b
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-02-13 19:36:20 +0530

    Update page.js

--- app/ai/page.js
+++ app/ai/page.js
@@ -5,7 +5,7 @@ import { Center, Square, Circle } from '@chakra-ui/react'
 import { Stack, VStack, Hstack, Box } from '@chakra-ui/react'
 export default function page() {
   return (
-    if (typeof window !== "undefined") {
+    
   // Client-side-only code
 
     <Stack spacing="10px">
@@ -26,5 +26,5 @@ export default function page() {
             
         </Box>
     </Stack>
-  })
+  )
 }
