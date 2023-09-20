d0119b8dd4b5c45bddcb53a26b3acbac68c42ac8
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-09-20 14:37:46 +0530

    Update page.js

--- app/my-designs/page.js
+++ app/my-designs/page.js
@@ -1,23 +1,41 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { VStack, Box, Button, Input, Text } from '@chakra-ui/react';
+import Typewriter from 'typewriter-effect';
 
 export default function QuestionAnsweringApp() {
   const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
 
+  const questions = [
+    "What's your name?",
+    "How does photosynthesis work?",
+    "Ask a question...",
+  ];
+
   // Simple question-answering function (replace with your logic)
   const getAnswer = (questionText) => {
     // Basic question-answering logic here (e.g., hardcoded answers)
     switch (questionText.toLowerCase()) {
-      case 'what is your name?':
+      case "what's your name?":
         return "My name is Assistant.";
       case 'how does photosynthesis work?':
-        return "Photosynthesis is the process by which plants convert sunlight into energy.";
+        return 'Photosynthesis is the process by which plants convert sunlight into energy.';
       default:
         return "I'm sorry, I don't know the answer to that question.";
     }
   };
+
+  const [introIndex, setIntroIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIntroIndex((prevIndex) =>
+        prevIndex === questions.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change text every 3 seconds
+    return () => clearInterval(intervalId);
+  }, []);
 
   const handleQuestionSubmit = () => {
     const answerText = getAnswer(question);
@@ -26,6 +44,16 @@ export default function QuestionAnsweringApp() {
 
   return (
     <VStack spacing="4" align="center">
+      {/* Animated Introduction */}
+      <Typewriter
+        options={{
+          strings: [questions[introIndex]],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+      <br />
+
       {/* Text Input */}
       <Input
         type="text"
