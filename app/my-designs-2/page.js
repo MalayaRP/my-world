091da039213ee292f7a0be6bf8516634c6242547
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-09-20 16:57:24 +0530

    Update page.js

--- app/my-designs-2/page.js
+++ app/my-designs-2/page.js
@@ -29,33 +29,52 @@ function App() {
   return (
     <ChakraProvider>
       <VStack spacing={4} align="center">
-        <Input
-          placeholder="Take a note..."
-          value={note}
-          onChange={handleNoteChange}
-          size="lg"
-          width="80%" // Adjust the input width
-        />
-        <Button onClick={handleSaveNote} colorScheme="teal" size="lg">
-          Save Note
-        </Button>
-        <UnorderedList>
-          {notes.map((savedNote, index) => (
-            <ListItem
-              key={index}
-              borderBottom="1px solid #ccc" // Add a border to separate notes
-              padding="1px" // Add some padding to notes
-              width="80%" // Adjust the note width
-            >
-              {savedNote}
-            </ListItem>
-          ))}
-        </UnorderedList>
+        <Text fontSize="2xl" fontWeight="bold">
+          My Note App
+        </Text>
+        <Box
+          boxShadow="lg"
+          p={4}
+          borderRadius="md"
+          width="80%"
+          borderWidth="1px"
+          borderColor="gray.200"
+        >
+          <Input
+            placeholder="Take a note..."
+            value={note}
+            onChange={handleNoteChange}
+            size="lg"
+          />
+          <Button
+            onClick={handleSaveNote}
+            colorScheme="teal"
+            size="lg"
+            mt={2}
+            width="100%"
+          >
+            Save Note
+          </Button>
+        </Box>
+        {notes.length > 0 && (
+          <UnorderedList width="80%" spacing={2} mt={4}>
+            {notes.map((savedNote, index) => (
+              <ListItem
+                key={index}
+                borderBottom="1px solid #ccc"
+                padding="4px"
+                borderRadius="md"
+                bg="white"
+                _hover={{ bg: 'gray.100' }}
+              >
+                {savedNote}
+              </ListItem>
+            ))}
+          </UnorderedList>
+        )}
       </VStack>
     </ChakraProvider>
   );
 }
 
 export default App;
-
-
