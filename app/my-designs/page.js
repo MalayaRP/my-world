8dbe4c3b4c29771f4973db83f59b5b8593da1a89
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-09-20 14:14:52 +0530

    Update page.js

--- app/my-designs/page.js
+++ app/my-designs/page.js
@@ -1,53 +1,48 @@
+import React, { useState } from 'react';
+import { VStack, Box, Button, Input, Text } from '@chakra-ui/react';
 
-import React from 'react';
-import Image from 'next/image';
-import { Stack, VStack, HStack, Box } from '@chakra-ui/react';
-import {
-  List,
-  ListItem,
-  ListIcon,
-  OrderedList,
-  UnorderedList,
-} from '@chakra-ui/react';
-import { CheckCircleIcon } from '@chakra-ui/icons';
+export default function QuestionAnsweringApp() {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
 
-export default function TextToImageSearchApp() {
+  // Simple question-answering function (replace with your logic)
+  const getAnswer = (questionText) => {
+    // Basic question-answering logic here (e.g., hardcoded answers)
+    switch (questionText.toLowerCase()) {
+      case 'what is your name?':
+        return "My name is Assistant.";
+      case 'how does photosynthesis work?':
+        return "Photosynthesis is the process by which plants convert sunlight into energy.";
+      default:
+        return "I'm sorry, I don't know the answer to that question.";
+    }
+  };
+
+  const handleQuestionSubmit = () => {
+    const answerText = getAnswer(question);
+    setAnswer(answerText);
+  };
+
   return (
     <VStack spacing="4" align="center">
       {/* Text Input */}
-      {/* Add your text input component here */}
+      <Input
+        type="text"
+        placeholder="Enter your question"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      />
 
-      {/* Image Result */}
-      <Box>
-        {/* Display the image result here */}
-        <Image
-          src={/* Add image source from the search */}
-          alt="Image Result"
-          width={400}
-          height={400}
-        />
-      </Box>
+      {/* Button to submit the question */}
+      <Button onClick={handleQuestionSubmit}>Ask</Button>
 
-      {/* List of Search Results */}
-      <List spacing={3}>
-        {/* Loop through and display search results */}
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          {/* Display search result text */}
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          {/* Display search result text */}
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          {/* Display search result text */}
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          {/* Display search result text */}
-        </ListItem>
-      </List>
+      {/* Display the answer */}
+      {answer && (
+        <Box>
+          <Text fontWeight="bold">Answer:</Text>
+          <Text>{answer}</Text>
+        </Box>
+      )}
     </VStack>
   );
 }
