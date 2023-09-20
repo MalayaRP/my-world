c8ac3c207ba5ebea3923b72f183e8fb2b53ab779
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-09-20 15:39:27 +0530

    Update page.js

--- app/my-designs/page.js
+++ app/my-designs/page.js
@@ -1,89 +1,51 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { VStack, Box, Button, Input, Text } from '@chakra-ui/react';
 
 export default function QuestionAnsweringApp() {
   const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
 
-  const questions = [
-    "What's your name?",
-    "How does photosynthesis work?",
-    "Ask a question...",
-  ];
+  // Function to fetch the answer
+  const fetchAnswer = () => {
+    const questionsAndAnswers = {
+      'What is the capital of France?': 'Paris',
+      'Who wrote "Romeo and Juliet"?': 'William Shakespeare',
+      'What is the largest planet in our solar system?': 'Jupiter',
+      'What is the boiling point of water in Celsius?': '100°C',
+      'How many continents are there on Earth?': '7',
+    };
 
-  // Simple question-answering function (replace with your logic)
-  const getAnswer = (questionText) => {
-    // Basic question-answering logic here (e.g., hardcoded answers)
-    switch (questionText.toLowerCase()) {
-      case "what's your name?":
-        return "My name is Assistant.";
-      case 'how does photosynthesis work?':
-        return 'Photosynthesis is the process by which plants convert sunlight into energy.';
-      default:
-        return "I'm sorry, I don't know the answer to that question.";
+    const foundAnswer = questionsAndAnswers[question];
+
+    if (foundAnswer) {
+      setAnswer(foundAnswer);
+    } else {
+      setAnswer('Answer not found for this question');
     }
   };
 
-  const [introIndex, setIntroIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIntroIndex((prevIndex) =>
-        prevIndex === questions.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000); // Change text every 3 seconds
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const handleQuestionSubmit = () => {
-    const answerText = getAnswer(question);
-    setAnswer(answerText);
-  };
-
   return (
-    <VStack spacing="4" align="center">
-      {/* Animated Introduction */}
-      <div className="animated-text">
-        {questions[introIndex]}
-      </div>
-      <br />
-
-      {/* Text Input */}
-      <Input
-        type="text"
-        placeholder="Enter your question"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-
-      {/* Button to submit the question */}
-      <Button onClick={handleQuestionSubmit}>Ask</Button>
-
-      {/* Display the answer */}
-      {answer && (
-        <Box>
-          <Text fontWeight="bold">Answer:</Text>
-          <Text>{answer}</Text>
-        </Box>
-      )}
-
-      <style jsx>{`
-        .animated-text {
-          font-size: 36px;
-          font-family: Arial, sans-serif;
-          animation: fade 3s infinite;
-        }
-
-        @keyframes fade {
-          0%, 100% {
-            opacity: 0;
-          }
-          50% {
-            opacity: 1;
-          }
-        }
-      `}</style>
+    <VStack spacing={4} align="center">
+      <Box>
+        <Text fontSize="xl">Question Answering Demo</Text>
+      </Box>
+      <Box>
+        <Input
+          placeholder="Ask a question..."
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+      </Box>
+      <Box>
+        <Button colorScheme="blue" onClick={fetchAnswer}>
+          Get Answer
+        </Button>
+      </Box>
+      <Box>
+        <Text fontSize="lg">Answer:</Text>
+        <Text>{answer}</Text>
+      </Box>
     </VStack>
   );
 }
