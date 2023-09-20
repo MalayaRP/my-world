82200c5c660115488452949c2aa5fa53121f490b
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-09-20 14:05:27 +0530

    Update page.js

--- app/my-designs/page.js
+++ app/my-designs/page.js
@@ -1,1 +1,53 @@
 
+import React from 'react';
+import Image from 'next/image';
+import { Stack, VStack, HStack, Box } from '@chakra-ui/react';
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from '@chakra-ui/react';
+import { CheckCircleIcon } from '@chakra-ui/icons';
+
+export default function TextToImageSearchApp() {
+  return (
+    <VStack spacing="4" align="center">
+      {/* Text Input */}
+      {/* Add your text input component here */}
+
+      {/* Image Result */}
+      <Box>
+        {/* Display the image result here */}
+        <Image
+          src={/* Add image source from the search */}
+          alt="Image Result"
+          width={400}
+          height={400}
+        />
+      </Box>
+
+      {/* List of Search Results */}
+      <List spacing={3}>
+        {/* Loop through and display search results */}
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          {/* Display search result text */}
+        </ListItem>
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          {/* Display search result text */}
+        </ListItem>
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          {/* Display search result text */}
+        </ListItem>
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          {/* Display search result text */}
+        </ListItem>
+      </List>
+    </VStack>
+  );
+}
