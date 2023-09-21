4e8e5e20485ebbb04d0132bf47616feac13b4b4f
Malaya Ranjan P <90465658+MalayaRP@users.noreply.github.com>
2023-09-21 08:04:07 +0530

    Update page.js

--- app/my-designs/page.js
+++ app/my-designs/page.js
@@ -1,33 +1,70 @@
 "use client"
 import React, { useState } from 'react';
-import { VStack, Box, Button, Input, Text, ChakraProvider } from '@chakra-ui/react';
+import {
+  VStack,
+  Box,
+  Button,
+  Input,
+  Text,
+  ChakraProvider,
+} from '@chakra-ui/react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 
 function App() {
-  // Array of random text options
-  const randomTextOptions = [
-    "Hello, world!",
-    "This is a random text.",
-    "Chakra UI is awesome!",
-    "Coding in React is fun!",
-    "Random text generator",
-    "Click again for more text.",
-  ];
+  const [data, setData] = useState([]);
+  const [xValue, setXValue] = useState('');
+  const [yValue, setYValue] = useState('');
 
-  const [randomText, setRandomText] = useState("Click the button to generate random text.");
-
-  const generateRandomText = () => {
-    const randomIndex = Math.floor(Math.random() * randomTextOptions.length);
-    const newText = randomTextOptions[randomIndex];
-    setRandomText(newText);
+  const handleAddData = () => {
+    if (xValue && yValue) {
+      setData([...data, { x: parseFloat(xValue), y: parseFloat(yValue) }]);
+      setXValue('');
+      setYValue('');
+    }
   };
 
   return (
     <ChakraProvider>
-      <VStack spacing={4} align="center">
-        <Text fontSize="24px">{randomText}</Text>
-        <Button onClick={generateRandomText} colorScheme="teal" size="lg">
-          Generate Random Text
-        </Button>
+      <VStack spacing={4} alignItems="center" marginTop="20px">
+        <Text fontSize="xl">Plotting App</Text>
+        <Box>
+          <Input
+            type="number"
+            placeholder="Enter X value"
+            value={xValue}
+            onChange={(e) => setXValue(e.target.value)}
+          />
+          <Input
+            type="number"
+            placeholder="Enter Y value"
+            value={yValue}
+            onChange={(e) => setYValue(e.target.value)}
+          />
+          <Button colorScheme="teal" onClick={handleAddData}>
+            Add Data Point
+          </Button>
+        </Box>
+        <LineChart
+          width={600}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="x" type="number" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="y" stroke="#8884d8" />
+        </LineChart>
       </VStack>
     </ChakraProvider>
   );
